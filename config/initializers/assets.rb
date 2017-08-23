bf15454acd3f81e1ec0573053951954fac8cb444
Camilo Garcia <gcgarciab@unal.edu.co>
2017-08-23 08:48:51 -0500

    Some changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( statics.css )
 Rails.application.config.assets.precompile += %w( admins.css )
+Rails.application.config.assets.precompile += %w( administration.css )
